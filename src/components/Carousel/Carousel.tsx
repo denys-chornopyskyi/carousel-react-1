d89469f866a771b5c1ebdb14a29f26ef4b7c421c
denys-chornopyskyi <chornopyskyidenys@gmail.com>
2025-06-04 23:06:05 +0200

    commit

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,235 +1,127 @@
-import {ArrowBigLeft, ArrowBigRight, Circle, CircleDot} from "lucide-react";
-import {useEffect, useRef, useState} from "react";
+import { ArrowBigLeft, ArrowBigRight, Circle, CircleDot } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 import "./Carousel.css";
-import type {settingsProps} from "../../App";
-import {wait} from "./utils/timeout.ts";
+import type { settingsProps } from "../../App";
+import { wait } from "./utils/timeout.ts";
+import {
+  CSSTransition,
+  TransitionGroup,
+  SwitchTransition,
+} from "react-transition-group";
+import React from "react";
 
 interface CarouselProps extends settingsProps {
   imageUrls: string[];
 }
 
-const Carousel = ({imageUrls, settings, setSettings}: CarouselProps) => {
+const Carousel = ({ imageUrls, settings, setSettings }: CarouselProps) => {
   const [imageIndex, setImageIndex] = useState<number>(0);
-  const [renderQueue, setRenderQueue] = useState<string[]>(
-    [imageUrls[imageIndex]])
+  const [renderQueue, setRenderQueue] = useState<string[]>([
+    imageUrls[imageIndex],
+  ]);
+
+const slideDirection = useRef<'next' | 'prev'>('prev')
 
 
-  const currentTarget = useRef<HTMLImageElement>(null)
-  const targetIndex = useRef<1 | 2 | 0>(0)
-  const [targetIndexState, setTargetIndexState] = useState(false)
 
-  const showPrevImage = () => {
-    let i = 0
-    setImageIndex((index) => {
-      if (index === 0) {
-        i = imageUrls.length - 1
-      } else {
-        i = index - 1
-      }
-      return i;
-    });
-    setRenderQueue((prevState) => {
-      const tmp = [...prevState];
-      for (let j = 0; j < imageUrls.length; j++) {
-        if (j === i) {
-          tmp[j] = imageUrls[j]
-          console.log(j)
-        }
-      }
-      console.log(tmp)
-
-      return tmp
-    })
-
-  };
-
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        async function loop() {
-          await wait(10000)
-          setRenderQueue((prevState) => {
-            if (targetIndex) {
-
-              // console.log(prevState.slice(1))
-              return prevState.slice(1)
-            } else {
-              return prevState.slice(0, 1)
-            }
-          })
-          observer.disconnect()
-        }
-
-        setRenderQueue((prevState) => {
-          if (!targetIndex.current) {
-
-            console.log('obrazenij 1', prevState.slice(1))
-            return prevState.slice(1)
-          } else {
-            console.log('obrazanij', prevState.slice(0, 1))
-            return prevState.slice(0, 1)
-          }
-        })
-        setSettings({...settings, animation: false})
-        setTargetIndexState(false)
-        observer.disconnect()
-
-
-      }
-    })
-  }, {
-    threshold: 0.05
-  })
-  // console.log(imageIndex)
-
-  const showNextImageStrip = () => {
-    setImageIndex((index) => {
-      if (index === imageUrls.length - 1) return 0;
-      return index + 1;
-    });
-
-  };
-  // const showNextImageDeque = () => {
-  //   let i = 0
-  //   setImageIndex((index) => {
-  //     if (index === imageUrls.length - 1) {
-  //       i = 0
-  //     } else {
-  //       i = index + 1
-  //     }
-  //     return i;
-  //   });
-  //   if (renderQueue.length !== imageUrls.length) {
-  //     setRenderQueue((prevState) => [...prevState, imageUrls[i + 1]])
-  //   }
-  //
-  // }
-
-  const showPrevImageDeque = () => {
-    targetIndex.current = 1
-    console.log(1)
-    let currentIndex = imageIndex;
-    setImageIndex((prevIndex) => {
-      if (prevIndex === 0) {
-        currentIndex = imageUrls.length - 1
-      } else {
-        currentIndex = prevIndex - 1
-      }
-      return currentIndex;
-    })
-
-    setRenderQueue((prevQueue) => [imageUrls[currentIndex], ...prevQueue])
+  const setSlide = (newIndex) => {
+    let tmpIndex = newIndex
+    
+    setImageIndex(tmpIndex)
+    setRenderQueue([imageUrls[tmpIndex]])
   }
 
+
   const showNextImageDeque = () => {
-    if (renderQueue.length > 1) return
-    targetIndex.current = 0
     let currentIndex = imageIndex;
     setImageIndex((prevIndex) => {
       if (prevIndex === imageUrls.length - 1) {
-        currentIndex = 0
+        currentIndex = 0;
       } else {
-        currentIndex = prevIndex + 1
+        currentIndex = prevIndex + 1;
       }
       return currentIndex;
-    })
-    setRenderQueue((prevQueue) => [...prevQueue, imageUrls[currentIndex]])
-  }
+    });
+    setRenderQueue((prevQueue) => [imageUrls[currentIndex]]);
+  };
 
-  console.log(renderQueue)
-  useEffect(() => {
-      let canceled = false;
+  console.log(renderQueue);
+ 
 
-      // async function loop() {
-      //   while (!canceled && settings.autoReplay) {
-      //     await wait(settings.autoReplayDelay)
-      //     showNextImageDeque()
-      //     if (settings.animation) {
-      //       await wait(settings.animDuration)
-      //     }
-      //   }
-      // }
+  const nodeRefs = useRef<{ [key: string]: React.RefObject<HTMLImageElement> }>(
+    {}
+  );
 
-      // console.log('currentTarget:', currentTarget.current)
-      // console.log('targetIndex:', targetIndex.current)
-
-      async function loop() {
-
-        if (renderQueue.length === 2) {
-          console.log(currentTarget.current)
-          if (currentTarget.current) observer.observe(currentTarget.current)
-          setTargetIndexState(true)
-          console.log('konec')
-        }
-
-      }
-
-      setSettings({...settings, animation: true})
-      loop()
-      // loop()
-      return () => {
-        canceled = true
-      }
-    },
-    [renderQueue])
-  // [settings.autoReplay, settings.animation]
-
+  const getNodeRef = (key: string): React.RefObject<HTMLImageElement> => {
+    if (!nodeRefs.current[key]) {
+      nodeRefs.current[key] = React.createRef<HTMLImageElement>();
+    }
+    return nodeRefs.current[key];
+  };
 
   return (
-    <div style={{position: "relative", width: "640px", height: "auto"}}>
-      <div
+    <div style={{ position: "relative", width: "640px", height: "auto" }}>
+      <TransitionGroup
         style={{
           display: "flex",
           overflow: "hidden",
           width: "100%",
           height: "100%",
+          position: 'relative',
         }}
       >
-        {/*{settings.sliderLogic === 'strip' && imageUrls.map((url) => {*/}
-        {/*  return <img*/}
-        {/*    alt={`img-${url}`}*/}
-        {/*    key={url}*/}
-        {/*    src={url}*/}
-        {/*    className="carousel-image"*/}
-        {/*    style={{*/}
-        {/*      translate: `${-100 * imageIndex}%`,*/}
-        {/*      transition: settings.animation ? `translate ${settings.animDuration}ms ${settings.animationCurve}` : ''*/}
-        {/*    }}*/}
-        {/*  />*/}
-        {/*})}*/}
-
-        {settings.sliderLogic === 'deque' && renderQueue.map((url, index) => (
-          <img
-            alt={`img-${url}`}
+        {renderQueue.map((url) => (
+          <CSSTransition
             key={url}
-            src={url}
-            ref={index === 0 ? currentTarget : null}
-            // ref={currentTarget}
-            className="carousel-image"
-            style={{
-              translate: targetIndexState ? `${-100 * (renderQueue.length - 1)}%` : ``,
-              transition: settings.animation ? `translate ${settings.animDuration}ms ${settings.animationCurve}` : ''
-            }}
-          />
+            classNames='fade'
+            nodeRef={getNodeRef(url)}
+            unmountOnExit
+            timeout={500}
+          >
+            <img
+              alt={`img-${url}`}
+              src={url}
+              ref={getNodeRef(url)}
+              className="carousel-image"
+            />
+          </CSSTransition>
         ))}
-      </div>
+      </TransitionGroup>
+
+      {/* {renderQueue.map((url, index) => (
+        <img
+          alt={`img-${url}`}
+          key={url}
+          src={url}
+          ref={index === 0 ? currentTarget : null}
+          // ref={currentTarget}
+          className="carousel-image"
+          style={{
+            translate: targetIndexState
+              ? `${-100 * (renderQueue.length - 1)}%`
+              : ``,
+            transition: settings.animation
+              ? `translate ${settings.animDuration}ms ${settings.animationCurve}`
+              : "",
+          }}
+        />
+      ))} */}
 
       {settings.addControlBtns && (
         <>
           <button
             className="carousel-btn"
-            onClick={showPrevImageDeque}
-            style={{left: 0}}
+            onClick={showNextImageDeque}
+            style={{ left: 0 }}
           >
-            <ArrowBigLeft/>
+            <ArrowBigLeft />
           </button>
           <button
             className="carousel-btn"
-            onClick={settings.sliderLogic === 'strip' ? showNextImageStrip : showNextImageDeque}
-            style={{right: 0}}
+            onClick={() => setSlide(imageIndex + 1)}
+            style={{ right: 0 }}
           >
-            <ArrowBigRight/>
+            <ArrowBigRight />
           </button>
         </>
       )}
@@ -243,15 +135,16 @@ const Carousel = ({imageUrls, settings, setSettings}: CarouselProps) => {
           gap: ".5rem",
         }}
       >
-        {settings.addIndicators && imageUrls.map((_, index) => (
-          <button
-            key={index}
-            className="carousel-dot-btn"
-            onClick={() => setImageIndex(index)}
-          >
-            {index === imageIndex ? <CircleDot/> : <Circle/>}
-          </button>
-        ))}
+        {settings.addIndicators &&
+          imageUrls.map((_, index) => (
+            <button
+              key={index}
+              className="carousel-dot-btn"
+              onClick={() => setImageIndex(index)}
+            >
+              {index === imageIndex ? <CircleDot /> : <Circle />}
+            </button>
+          ))}
       </div>
     </div>
   );
---
+++ src/components/Carousel/copyCarousel.tsx
@@ -0,0 +1,278 @@
+import { ArrowBigLeft, ArrowBigRight, Circle, CircleDot } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import "./Carousel.css";
+import type { settingsProps } from "../../App";
+import { wait } from "./utils/timeout.ts";
+import {
+  CSSTransition,
+  TransitionGroup,
+  SwitchTransition,
+} from "react-transition-group";
+import React from "react";
+
+interface CarouselProps extends settingsProps {
+  imageUrls: string[];
+}
+
+const Carousel = ({ imageUrls, settings, setSettings }: CarouselProps) => {
+  const [imageIndex, setImageIndex] = useState<number>(0);
+  const [renderQueue, setRenderQueue] = useState<string[]>([
+    imageUrls[imageIndex],
+  ]);
+
+  const currentTarget = useRef<HTMLImageElement>(null);
+  const targetIndex = useRef<1 | 2 | 0>(0);
+  const [targetIndexState, setTargetIndexState] = useState(false);
+
+  const showPrevImage = () => {
+    let i = 0;
+    setImageIndex((index) => {
+      if (index === 0) {
+        i = imageUrls.length - 1;
+      } else {
+        i = index - 1;
+      }
+      return i;
+    });
+    setRenderQueue((prevState) => {
+      const tmp = [...prevState];
+      for (let j = 0; j < imageUrls.length; j++) {
+        if (j === i) {
+          tmp[j] = imageUrls[j];
+          console.log(j);
+        }
+      }
+      console.log(tmp);
+
+      return tmp;
+    });
+  };
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          async function loop() {
+            await wait(10000);
+            setRenderQueue((prevState) => {
+              if (targetIndex) {
+                // console.log(prevState.slice(1))
+                return prevState.slice(1);
+              } else {
+                return prevState.slice(0, 1);
+              }
+            });
+            console.log('lox');
+            
+            observer.disconnect();
+          }
+
+          setRenderQueue((prevState) => {
+            if (!targetIndex.current) {
+              console.log("obrazenij 1", prevState.slice(1));
+              return prevState.slice(1);
+            } else {
+              console.log("obrazanij", prevState.slice(0, 1));
+              return prevState.slice(0, 1);
+            }
+          });
+          setSettings({ ...settings, animation: false });
+          setTargetIndexState(false);
+          observer.disconnect();
+        }
+      });
+    },
+    {
+      threshold: 0.05,
+    }
+  );
+
+  const showNextImageStrip = () => {
+    setImageIndex((index) => {
+      if (index === imageUrls.length - 1) return 0;
+      return index + 1;
+    });
+  };
+  // const showNextImageDeque = () => {
+  //   let i = 0
+  //   setImageIndex((index) => {
+  //     if (index === imageUrls.length - 1) {
+  //       i = 0
+  //     } else {
+  //       i = index + 1
+  //     }
+  //     return i;
+  //   });
+  //   if (renderQueue.length !== imageUrls.length) {
+  //     setRenderQueue((prevState) => [...prevState, imageUrls[i + 1]])
+  //   }
+  //
+  // }
+
+  const showPrevImageDeque = () => {
+    targetIndex.current = 1;
+    console.log(1);
+    let currentIndex = imageIndex;
+    setImageIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        currentIndex = imageUrls.length - 1;
+      } else {
+        currentIndex = prevIndex - 1;
+      }
+      return currentIndex;
+    });
+
+    setRenderQueue((prevQueue) => [imageUrls[currentIndex], ...prevQueue]);
+  };
+
+  const showNextImageDeque = () => {
+    // if (renderQueue.length > 1) return;
+    // targetIndex.current = 0;
+    let currentIndex = imageIndex;
+    setImageIndex((prevIndex) => {
+      if (prevIndex === imageUrls.length - 1) {
+        currentIndex = 0;
+      } else {
+        currentIndex = prevIndex + 1;
+      }
+      return currentIndex;
+    });
+    setRenderQueue((prevQueue) => [imageUrls[currentIndex]]);
+  };
+
+  console.log(renderQueue);
+  useEffect(() => {
+    let canceled = false;
+
+    // async function loop() {
+    //   while (!canceled && settings.autoReplay) {
+    //     await wait(settings.autoReplayDelay)
+    //     showNextImageDeque()
+    //     if (settings.animation) {
+    //       await wait(settings.animDuration)
+    //     }
+    //   }
+    // }
+
+    // console.log('currentTarget:', currentTarget.current)
+    // console.log('targetIndex:', targetIndex.current)
+
+    async function loop() {
+      if (renderQueue.length === 2) {
+        console.log(currentTarget.current);
+        if (currentTarget.current) observer.observe(currentTarget.current);
+        setTargetIndexState(true);
+        console.log("konec");
+      }
+    }
+
+    setSettings({ ...settings, animation: true });
+    loop();
+
+    return () => {
+      canceled = true;
+    };
+  }, [renderQueue]);
+
+  const nodeRefs = useRef<{ [key: string]: React.RefObject<HTMLImageElement> }>(
+    {}
+  );
+
+  const getNodeRef = (key: string): React.RefObject<HTMLImageElement> => {
+    if (!nodeRefs.current[key]) {
+      nodeRefs.current[key] = React.createRef<HTMLImageElement>();
+    }
+    return nodeRefs.current[key];
+  };
+
+  return (
+    <div style={{ position: "relative", width: "640px", height: "auto" }}>
+      <TransitionGroup
+        style={{
+          display: "flex",
+          overflow: "hidden",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {renderQueue.map((url) => (
+          <CSSTransition
+            key={url}
+            classNames="fade"
+            nodeRef={getNodeRef(url)}
+            unmountOnExit
+            timeout={5000}
+          >
+            <img
+              alt={`img-${url}`}
+              src={url}
+              ref={getNodeRef(url)}
+              className="carousel-image"
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+
+      {/* {renderQueue.map((url, index) => (
+        <img
+          alt={`img-${url}`}
+          key={url}
+          src={url}
+          ref={index === 0 ? currentTarget : null}
+          // ref={currentTarget}
+          className="carousel-image"
+          style={{
+            translate: targetIndexState
+              ? `${-100 * (renderQueue.length - 1)}%`
+              : ``,
+            transition: settings.animation
+              ? `translate ${settings.animDuration}ms ${settings.animationCurve}`
+              : "",
+          }}
+        />
+      ))} */}
+
+      {settings.addControlBtns && (
+        <>
+          <button
+            className="carousel-btn"
+            onClick={showPrevImageDeque}
+            style={{ left: 0 }}
+          >
+            <ArrowBigLeft />
+          </button>
+          <button
+            className="carousel-btn"
+            onClick={showNextImageDeque}
+            style={{ right: 0 }}
+          >
+            <ArrowBigRight />
+          </button>
+        </>
+      )}
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          translate: "-50%",
+          bottom: ".5rem",
+          display: "flex",
+          gap: ".5rem",
+        }}
+      >
+        {settings.addIndicators &&
+          imageUrls.map((_, index) => (
+            <button
+              key={index}
+              className="carousel-dot-btn"
+              onClick={() => setImageIndex(index)}
+            >
+              {index === imageIndex ? <CircleDot /> : <Circle />}
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
